92ae95a7e2b2699658afdc6a08e7cc319fe46d72
Land Lu <v-luting@microsoft.com>
2019-02-22 14:33:10 +0800

    Add project files.

---
+++ CrossFireBaseDemo/CrossFireBaseDemo/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Plugin.FirebasePushNotification;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace CrossFireBaseDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            CrossFirebasePushNotification.Current.Subscribe("general");
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"TOKEN REC: {p.Token}");
+            };
+            System.Diagnostics.Debug.WriteLine($"TOKEN: {CrossFirebasePushNotification.Current.Token}");
+
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+                Console.WriteLine(Xamarin.Forms.Application.Current.Properties["Token"]);
+                MainPage.Navigation.PushAsync(new SecondPage());
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+            {
+                //System.Diagnostics.Debug.WriteLine(p.Identifier);
+
+                System.Diagnostics.Debug.WriteLine("Opened");
+                //Console.WriteLine(s.To)
+
+                MainPage.Navigation.PushAsync(new SecondPage());
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Action");
+
+                if (!string.IsNullOrEmpty(p.Identifier))
+                {
+                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
+                    foreach (var data in p.Data)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                    }
+
+                }
+
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationDeleted += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Dismissed");
+            };
+
+        }
+    }
+}
